4dc6a7eff202d2ab8ba2d7a49ff39f8cafb0b52a
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-28 11:06:47 +0200

    Для BookInstance добавлено разделение на секции. Каждая секция имеет свой заголовок (или None, если заголовок не нужен) и ассоциированный кортеж полей в словаре - формат сложный для описания, но относительно простой для понимания

--- catalog/admin.py
+++ catalog/admin.py
@@ -17,6 +17,14 @@ class LanguageAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 
 # todo Выдает ошибку, когда пытаюсь добавить вывод жанра в админ панель.
