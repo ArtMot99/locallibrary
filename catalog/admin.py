60338ef95fcb7be9f0b705e2e993795c1e1137f2
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-28 10:17:33 +0200

    отображена более интересная информация о каждом авторе, использован list_display

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-from .models import Book, Genre, BookInstance, Author
+from .models import Book, Genre, BookInstance, Author, Language
 
 
 # Регистриция моделей в админ панели
-admin.site.register(Book)
+# admin.site.register(Book)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
-admin.site.register(Author)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+admin.site.register(Language)
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+admin.site.register(Author, AuthorAdmin)
