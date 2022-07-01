ffde28b341ff4fc1e9eb916eed77cb27973322ce
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-01 10:15:34 +0200

    Применены сессии для улучшения взаимодействия с анонимным пользователем.

--- catalog/views.py
+++ catalog/views.py
@@ -18,6 +18,9 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
     num_genre = Genre.objects.count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
@@ -26,7 +29,8 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_genre': num_genre
+        'num_genre': num_genre,
+        'num_visits': num_visits,
     })
 
 
